2015213a5fbada74b56a3557e8bb105bdeffdbb2
John Chen <john@johnchen.co>
2016-09-13 00:33:22 -0400

    test showcard count

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,12 +1,19 @@
 const { expect } = require('chai');
 const React = require('react');
-const Search = require('../js/Search');
+const Search = require('../js/search');
+const ShowCard = require('../js/showcard');
 const { shallow } = require('enzyme');
+const { shows } = require('../public/data');
 
 describe('<Search />', () => {
 	it('should render the brand', () => {
 		const wrapper = shallow(<Search/>);
-		console.log(wrapper.debug());
 		expect(wrapper.contains(<h1 className="brand">svideo</h1>)).to.be.true;
+	});
+
+	it('should render as many shows as they are data for', () => {
+		const wrapper = shallow(<Search/>);
+		
+		expect(wrapper.find(ShowCard).length).to.equal(shows.length);
 	});
 });
